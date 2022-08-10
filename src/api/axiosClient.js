c5b0dab176b18e764b298587dd6738270bb677c5
The-Flying-Dev <leandranm@outlook.com>
2022-08-10 16:00:39 -0700

    Add indentation to api folder files

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -4,24 +4,24 @@ import queryString from 'query-string';
 import apiConfig from "./apiConfig";
 
 const axiosClient = axios.create({
-  baseURL: apiConfig.baseUrl,
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  //optional function for serializing params
-  paramsSerializer: params => queryString.stringify({...params, api_key: apiConfig.apiKey})
+    baseURL: apiConfig.baseUrl,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    //optional function for serializing params
+    paramsSerializer: params => queryString.stringify({...params, api_key: apiConfig.apiKey})
 });
 
 axiosClient.interceptors.request.use(async (config) => config);
 
 //checks data is valid before returning
 axiosClient.interceptors.response.use((response) => {
-  if (response && response.data) {
-    return response.data;
-  }
-  return response;
-}, (error) => {
-    throw error;
+    if (response && response.data) {
+      return response.data;
+    }
+    return response;
+  }, (error) => {
+      throw error;
 });
 
 export default axiosClient;
